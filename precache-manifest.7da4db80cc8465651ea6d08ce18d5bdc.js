d84b5d4ceb8c4470ef62061209941d742a1419bb
Your Name <m.meeran.nizarudeen@gmail.com>
2021-12-24 09:49:28 +0530

    Updates

---
+++ precache-manifest.7da4db80cc8465651ea6d08ce18d5bdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e421796fbcda21a8e87",
+    "url": "/reactapp/static/css/main.0bebf1be.chunk.css"
+  },
+  {
+    "revision": "9e421796fbcda21a8e87",
+    "url": "/reactapp/static/js/main.9e421796.chunk.js"
+  },
+  {
+    "revision": "dbf16a7a1f5d12f53526",
+    "url": "/reactapp/static/js/1.dbf16a7a.chunk.js"
+  },
+  {
+    "revision": "b75323fd636df612d81b",
+    "url": "/reactapp/static/js/2.b75323fd.chunk.js"
+  },
+  {
+    "revision": "9720c5652e1c7c5a675c",
+    "url": "/reactapp/static/js/runtime~main.9720c565.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/reactapp/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "acb79286c3a0a4d6b3dbc41ae4593435",
+    "url": "/reactapp/index.html"
+  }
+];
